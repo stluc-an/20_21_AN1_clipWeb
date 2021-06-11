48aff6c1b5fb5188436e750d1897331e199c4a99
LouGodfroid <78653539+LouGodfroid@users.noreply.github.com>
2021-06-11 14:49:52 +0200

    maj finale

--- clips/godfroid_louane/src/ImageDisplayer.js
+++ clips/godfroid_louane/src/ImageDisplayer.js
@@ -8,10 +8,25 @@ class ImageDisplayer{
 		this.w=w;
 		this.h=h;
 		this.isVisible = false;
+        this.isDancing = false;
+        this.isDancingW = false;
+        this.isDancingH = false;
 	}
 	draw(){
 		if(this.isVisible){
 		
+            if(this.isDancing){
+                this.w = random(70);
+                this.h = random(70);
+            }
+            
+            if(this.isDancingW){
+                this.w = random(60);
+            }
+            
+            if(this.isDancingH){
+                this.h = random(60);
+            }
 			image(this.img, this.x,  this.y,  this.w,  this.h);
 		}
 	}
